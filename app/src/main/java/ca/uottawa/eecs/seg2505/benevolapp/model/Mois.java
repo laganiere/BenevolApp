9397127c02f337deabb817ea72907aa189b79b1f
Robert Laganiere <robert@laganiere.name>
2015-11-25 08:51:39 -0500

    mois et mainactivity

--- app/src/main/java/ca/uottawa/eecs/seg2505/benevolapp/model/Mois.java
+++ app/src/main/java/ca/uottawa/eecs/seg2505/benevolapp/model/Mois.java
@@ -12,7 +12,7 @@ public enum Mois {
     }
 
     public static Mois get(int index) {
-        if (index < 0) return null;
+        if (index < 0 || index > 12) return null;
         for (Mois m : values()) if (m.getIndex() == index) return m;
         return null;
     }
