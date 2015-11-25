6447c8d090be054cb25ec1ec19a5028bb55b6b2f
Robert Laganiere <robert@laganiere.name>
2015-11-25 09:14:16 -0500

    ajout d'un organisme au login

--- app/src/main/java/ca/uottawa/eecs/seg2505/benevolapp/MainOrganismeActivity.java
+++ app/src/main/java/ca/uottawa/eecs/seg2505/benevolapp/MainOrganismeActivity.java
@@ -6,12 +6,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import ca.uottawa.eecs.seg2505.benevolapp.controlleur.Delegateur;
+import ca.uottawa.eecs.seg2505.benevolapp.model.Organisme;
+
 public class MainOrganismeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_organisme);
+
+        Delegateur.utilisateurCourant= new Organisme("Croix rouge",
+                "340",
+                "Catherine Stree",
+                "",
+                "K1R 1C4",
+                "Ottawa",
+                "613 560-7220",
+                "http://www.croixrouge.ca",
+                "rh@croixrouge.ca",
+                "La Croix-Rouge canadienne vise à améliorer les conditions d’existence des personnes vulnérables en mobilisant le pouvoir de l’humanité au Canada et partout dans le monde.",
+                "Grande",
+                "Aide humanitaire",
+                "");
     }
 
     public void onCancel(View view) {
